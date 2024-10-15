0a29a0385a686025e9dd6e411a98fa2f36be4c78
rmagnago <rmagnago@outlook.com>
2024-10-14 23:08:50 -0300

    feat: sidebar

---
+++ src/app/models/classe.ts
@@ -0,0 +1,6 @@
+export interface Classe {
+    id: string;
+    nome: string;
+    valor: number;
+    prazoDevolucao: number;
+}
---
+++ src/app/models/diretor.ts
@@ -0,0 +1,4 @@
+export interface Diretor {
+    id: string;
+    nome: string;
+}
