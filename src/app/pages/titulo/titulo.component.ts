d83f2d90fa53ce8bc90693d39566dfc48c963357
rmagnago <rmagnago@outlook.com>
2024-11-09 00:33:58 -0300

    feat: Atualiza componente de formulário de título

--- src/app/pages/titulo/titulo.component.ts
+++ src/app/pages/titulo/titulo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TituloService } from '../../services/titulo';
 import { Titulo } from '../../models/titulo';
 import { FormsModule } from '@angular/forms';
@@ -18,6 +18,8 @@ import { SelectAtoresComponent } from "../../components/titulo/select-atores/sel
   standalone: true
 })
 export class TituloFormComponent implements OnInit {
+  @ViewChild(SelectAtoresComponent) selectAtoresComponent!: SelectAtoresComponent;
+
   nome: string = '';
   ano: number = 0;
   sinopse: string = '';
@@ -28,17 +30,46 @@ export class TituloFormComponent implements OnInit {
   titulos!: Titulo[];
 
   constructor(private tituloService: TituloService, public dialog: MatDialog) { }
+
   ngOnInit(): void {
-    this.tituloService.getTitulos().subscribe((resposta) => {
-      this.titulos = resposta;
-      console.log('Titulos carregados:', this.titulos);
-    })
+    this.carregarTitulos();
   }
 
   carregarTitulos(): void {
     this.tituloService.getTitulos().subscribe((resposta) => {
       this.titulos = resposta;
     });
+  }
+
+  salvarTitulo() {
+    if (this.nome && this.diretor && this.classe && this.atores.length > 0) {
+      const novoTitulo: Titulo = {
+        nome: this.nome,
+        ano: this.ano,
+        sinopse: this.sinopse,
+        categoria: this.categoria,
+        atores: this.atores,
+        diretor: this.diretor,
+        classe: this.classe,
+      };
+
+      this.tituloService.criarTitulo(novoTitulo).subscribe(() => {
+        this.nome = '';
+        this.ano = 0;
+        this.sinopse = '';
+        this.categoria = '';
+        this.diretor = undefined!;
+        this.classe = undefined!;
+        this.atores = [];
+        alert('Titulo salvo com sucesso!');
+        this.ngOnInit();
+        this.selectAtoresComponent.formControl.setValue([]);
+      }, (error) => {
+        console.error('Erro ao salvar título', error);
+      });
+    } else {
+      alert('Por favor, preencha todos os campos obrigatórios');
+    }
   }
 
   atualizarTitulo(titulo: Titulo): void {
@@ -57,46 +88,13 @@ export class TituloFormComponent implements OnInit {
     }
   }
 
-  salvarTitulo() {
-    if (this.nome && this.diretor && this.classe && this.atores.length > 0) {
-      const novoTitulo: Titulo = {
-        nome: this.nome,
-        ano: this.ano,
-        sinopse: this.sinopse,
-        categoria: this.categoria,
-        atores: this.atores,
-        diretor: this.diretor,
-        classe: this.classe,
-      };
-      console.log("Titulo: ", novoTitulo);
-
-      this.tituloService.criarTitulo(novoTitulo).subscribe(() => {
-        this.nome = '';
-        this.ano = 0;
-        this.sinopse = '';
-        this.categoria = '';
-        this.diretor = undefined!;
-        this.classe = undefined!;
-        this.atores = [];
-        alert('Titulo salvo com sucesso!');
-        this.ngOnInit();
-      }, (error) => {
-        console.error('Erro ao salvar título', error);
-      });
-    } else {
-      alert('Por favor, preencha todos os campos obrigatórios');
-    }
-  }
-
   trackByTitulo(index: number, item: Titulo): string {
     return item.id!;
   }
 
   formatarNomesAtores(atores: Ator[]): string {
     return atores && atores.length > 0
-        ? atores.map(ator => ator.nome).join(', ')
-        : 'Sem atores';
-}
-
-
+      ? atores.map(ator => ator.nome).join(', ')
+      : 'Sem atores';
+  }
 }
